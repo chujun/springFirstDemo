df4a711a3c97bc58192d693906c9d2670a0550ff
chujun <chujunhistory@163.com>
2015-08-12 19:53:11 +0800

    创建表演舞台idol和杂技表演者duke

---
+++ src/main/java/com/chujun/spring/demo/first/main/IdolMain.java
@@ -0,0 +1,21 @@
+/*
+* Copyright (C) 2006-2015 Tuniu All rights reserved
+* Author: chujun
+* Date: 2015-8-12
+* Description: 
+*/
+package com.chujun.spring.demo.first.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.chujun.spring.demo.first.service.Knight;
+import com.chujun.spring.demo.first.service.Performer;
+
+public class IdolMain {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-idol.xml");
+		Performer performer =(Performer) context.getBean("duke");
+		performer.perform();
+	}
+}
---
+++ src/main/java/com/chujun/spring/demo/first/service/impl/Juggler.java
@@ -0,0 +1,35 @@
+/*
+* Copyright (C) 2006-2015 Tuniu All rights reserved
+* Author: chujun
+* Date: 2015-8-12
+* Description: 
+*/
+package com.chujun.spring.demo.first.service.impl;
+
+import com.chujun.spring.demo.first.service.Performer;
+
+/**
+ * @author chujun
+ * 变戏法者（杂技表演）
+ */
+public class Juggler implements Performer{
+	
+	/**
+	 * 豆袋
+	 */
+	private int beanBags=3;
+	
+	public Juggler() {
+		super();
+	}
+
+	public Juggler(int beanBags) {
+		super();
+		this.beanBags = beanBags;
+	}
+
+	public void perform() {		
+		System.out.println("juggler"+beanBags+" beanBags");
+	}
+
+}
