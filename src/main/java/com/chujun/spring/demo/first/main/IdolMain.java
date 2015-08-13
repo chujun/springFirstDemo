339700b21579d613a365a50fd8a38532c7dd07fd
chujun <chujunhistory@163.com>
2015-08-13 14:00:25 +0800

    spring单例Bean与java类加载器单例区别

--- src/main/java/com/chujun/spring/demo/first/main/IdolMain.java
+++ src/main/java/com/chujun/spring/demo/first/main/IdolMain.java
@@ -24,5 +24,17 @@ public class IdolMain {
 		Ticket oneTicket =(Ticket) context.getBean("ticket");
 		Ticket twoTicket =(Ticket) context.getBean("ticket");
 		System.out.println(oneTicket==twoTicket);//true
+		//自己new出来的Bean
+		Ticket newTicket=new Ticket();
+		//自己new出来的bean和spring用getBean中获得单例Bean不是同一个实例
+		System.out.println(oneTicket==newTicket);//false
+		//一种特殊情况，spring容器声明了两个Ticket
+	/*	<!--bean作用域prototype-->
+		<bean id="ticket" class="com.chujun.spring.demo.first.domain.Ticket" scope="singleton"></bean>
+		<!--Ticket ticket=new Ticket();-->
+		<bean id="twoTicket" class="com.chujun.spring.demo.first.domain.Ticket" scope="singleton"></bean>
+		<!--Ticket twoTicket=new Ticket();//所以两次声明出来的spring单例Bean不是同一个实例-->*/
+		Ticket tTicket=(Ticket)context.getBean("twoTicket");
+		System.out.println(oneTicket==tTicket);//false
 	}
 }
