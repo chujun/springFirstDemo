c6cdbe26757ed1c1ff3fbd9c24b160a48539a99f
chujun <chujunhistory@163.com>
2015-08-13 12:32:27 +0800

    线程安全单例，利用内部类实现单例模式

---
+++ src/main/java/com/chujun/spring/demo/first/service/Stage.java
@@ -0,0 +1,18 @@
+package com.chujun.spring.demo.first.service;
+
+/**
+ * Created by chujun on 2015/8/13.
+ * 单例舞台：
+ * 静态工厂模式：处于线程安全考虑，采用了initialization on demand holder技术创建单例
+ */
+public class Stage {
+    public Stage() {
+    }
+    private static class StageSingletonHolder{
+        //延时加载实例
+        static Stage instance=new Stage();
+    }
+    public Stage getInstance(){
+        return StageSingletonHolder.instance;
+    }
+}
