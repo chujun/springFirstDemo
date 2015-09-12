5c96057291d69c864351e6928b4265a528835a70
chujun <1571970439@qq.com>
2015-09-12 14:33:01 +0800

    日常提交

---
+++ src/main/java/com/chujun/spring/demo/first/domain/Knife.java
@@ -0,0 +1,10 @@
+package com.chujun.spring.demo.first.domain;
+
+import org.springframework.context.annotation.Scope;
+
+/**
+ * Created by Administrator on 2015/9/2 0002.
+ */
+@Scope("prototype")
+public class Knife {
+}
