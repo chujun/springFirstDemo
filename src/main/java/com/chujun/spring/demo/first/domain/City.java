ba3b5ae598beb03edc1a02926891b0133c0efe4e
chujun <1571970439@qq.com>
2015-09-12 17:15:49 +0800

    util:list 列表数据类型创建完成，准备数据用于SpEL表达式筛选集合

---
+++ src/main/java/com/chujun/spring/demo/first/domain/City.java
@@ -0,0 +1,36 @@
+package com.chujun.spring.demo.first.domain;
+
+/**
+ * Created by Administrator on 2015/9/12 0012.
+ */
+public class City {
+    private String name;
+
+    private String state;
+
+    private int population;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
